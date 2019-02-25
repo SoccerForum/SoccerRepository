b9ebc3e9b6c6b99fceff7034c305e16ec77e4386
zongxinqi <32828039+zongxinqi@users.noreply.github.com>
2019-02-25 23:47:23 +0800

    2/25

--- SoccerForum/src/main/java/com/test/SoccerForum/dao/UserDao.java
+++ SoccerForum/src/main/java/com/test/SoccerForum/dao/UserDao.java
@@ -3,5 +3,5 @@ package com.test.SoccerForum.dao;
 import com.test.SoccerForum.domain.po.User;
 
 public interface UserDao extends GenericDao<User, Integer>{
-
+	
 }
--- SoccerForum/src/main/java/com/test/SoccerForum/web/controller/TestController.java
+++ SoccerForum/src/main/java/com/test/SoccerForum/web/controller/TestController.java
@@ -7,22 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.test.SoccerForum.dao.UserDao;
 import com.test.SoccerForum.domain.po.Member;
+import com.test.SoccerForum.domain.po.User;
 import com.test.SoccerForum.service.MemberService;
+import com.test.SoccerForum.service.UserService;
 
 
 @Controller
 public class TestController {
 	@Autowired
 	MemberService memberService;
-	
+	@Autowired
+	UserService userService;
 	@RequestMapping("/test")
-	public String toTest(Model model){
+	public String toTest(User user,Model model){
 		
-		List<Member> members = memberService.getAll() ;
+		List<Member> members = memberService.getAll();
 		
 		model.addAttribute("members",members);
 		
+		int res = userService.insertOne(user);
+		
+		System.out.println(res);
 		return "test";
 	}
 	@RequestMapping("/login")
